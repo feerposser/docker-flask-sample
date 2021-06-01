9f9d71d6cba373b651c80d1da98850dee7b9487f
Fernando Posser Pinheiro <fernando.posser@hotmail.com>
2021-05-31 22:22:15 -0300

    .

--- Dockerfile
+++ Dockerfile
@@ -11,5 +11,3 @@ RUN pip install -r requirements.txt
 COPY app.py .
 
 EXPOSE 5000
-
-CMD python app.py
--- app.py
+++ app.py
@@ -1,15 +1,38 @@
-from flask import Flask
+import os
+import datetime
+
+from flask import Flask, jsonify
+from flask_mongoengine import MongoEngine
+
 
 app = Flask(__name__)
 
+app.config["MONGODB_SETTINGS"] = {
+    "db": os.getenv("DATABASE_NAME", "atlecica"),
+    "host": os.getenv("DATABASE_HOST", "mongodb-atletica"),
+    "username": os.getenv("DATABASE_USER", "admin"),
+    "password": os.getenv("DATABASE_PASSWORD", "admin"),
+    'authentication_source': 'admin'
+}
+
+db = MongoEngine()
+
+class EventoAtletica(db.Document):
+    nome = db.StringField(required=True)
+    descricao = db.StringField()
+    organizadores = db.ListField()
+    participantes = db.ListField()
+    localizacao = db.DictField()
+    data = db.DateTimeField(default=datetime.datetime.now)
+
+    def __str__(self):
+        return self.nome
+
+db.init_app(app)
 
 @app.route("/")
 def index():
-    return "Deu certoooo"
-
-@app.route("/teste")
-def teste():
-    return "Este também deu certo"
+    return jsonify(EventoAtletica.objects.all())
 
 @app.route("/<nome>")
 def nome(nome):
