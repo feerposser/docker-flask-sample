69ac9e04122aa7b172b5ce59018a575cc4bd7af8
Fernando Posser Pinheiro <fernando.posser@hotmail.com>
2021-10-21 16:49:58 -0300

    updating for gdg

--- app.py
+++ app.py
@@ -5,15 +5,15 @@ app = Flask(__name__)
 
 @app.route("/")
 def index():
-    return "Deu certoooo"
+    return "Olá, mundo! Participe do Google Developer Groups Passo Fundo -> <a href='https://www.instagram.com/gdg.pf/' target='_BLANK'>clicando aqui.</a>"
 
 @app.route("/teste")
 def teste():
-    return "Este também deu certo"
+    return "Você também pode participar direto <a href='https://www.youtube.com/watch?v=oT-4we1FMPI' target='_BLANK'>clicando aqui</a>"
 
 @app.route("/<nome>")
 def nome(nome):
-    return "Deu certo, {}".format(nome)
+    return "{}, não esqueça que dia 23 e 30 do 10 tem GDG Passo Fundo!".format(nome)
 
 
 if __name__ == "__main__":
